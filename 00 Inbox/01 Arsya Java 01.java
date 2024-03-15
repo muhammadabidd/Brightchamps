8f3fca77d416f90879d307c0abdee896b39a14d8
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-15 18:02:17 +0700

    test

---
+++ 00 Inbox/01 Arsya Java 01.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) {
+        String text = "Halo semuanya, hari ini saya belajar java";
+        
+        System.out.println(text);
+        
+        
+        
+        String text1 = "Nama saya adalah ...";
+        String text2 = "Muhammad Abid";
+        
+        System.out.println(text1 + text2);
+    }
+}
