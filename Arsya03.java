1d715f6f682bcd82c4fd103e4a1bdf41e386cebb
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-19 17:01:41 +0700

    Arsya 19 Mar

---
+++ Arsya03.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+class Student{
+    String name, hobby;
+    int umur;
+}
+
+
+
+public class Arsya03 {
+    public static void main(String[] args) {
+        Scanner pembacainput = new Scanner(System.in);
+
+        System.out.println("Masukkan Nama anda di bawah ini:");
+        String nama = pembacainput.next();
+
+
+
+        System.out.println("Nama Saya adalah " + nama);
+
+
+
+
+
+
+        System.out.println("----------------");
+        Student student1 =  new Student();
+
+        student1.umur = pembacainput.nextInt();
+
+        student1.name = "Ujang";
+        System.out.println(student1.name);
+
+        pembacainput.close();
+    }
+    
+
+}
