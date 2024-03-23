1c5d1a7965d445b70be1a7a67b9e40f18021e4af
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-23 16:07:35 +0700

    sabtu

---
+++ Arsya05.java
@@ -0,0 +1,50 @@
+public class Arsya05 {
+
+    public static void main(String[] args) {
+        System.out.println("Testing dulu ");
+
+        int hari = 123123123;
+
+
+        // if (hari ==1){
+        //     System.out.println("Senin");
+        // }
+
+
+
+        switch (hari){
+            case 1:
+                System.out.println("Hari Senin");
+                break;
+
+            case 2:
+                System.out.println("Hari Selasa");
+                break;
+
+            case 3:
+                System.out.println("Hari Rabu");
+                break;
+
+            case 4:
+                System.out.println("Hari Kamis");
+                break;
+
+            case 5:
+                System.out.println("Hari Jumat");
+                break;
+
+            case 6:
+                System.out.println("Hari Sabtu");
+                break;
+
+            case 7:
+                System.out.println("Hari Minggu");
+                break;
+
+            default:
+                System.out.println("SALAH WOI");
+        }
+
+
+    }
+}
