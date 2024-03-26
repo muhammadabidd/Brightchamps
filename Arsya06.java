46553704de00dee2efbaf0ad54ea1cbbc47a054a
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-26 17:04:06 +0700

    Arsya - Tuesday

---
+++ Arsya06.java
@@ -0,0 +1,42 @@
+public class Arsya06 {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Muhammad Abid");
+
+
+        // int umur = 5;
+
+        // if (umur > 17){
+        //     System.out.println("ENTE BOLEH MENGENDARAHI");
+        // }
+        // else {
+        //     System.out.println("ENTE BELUM BOLEH MENGENDARAHI");
+        // }
+
+
+        // String hasil = (umur > 17)?"KAMU BOLEH MENGENDARAI SUPRA":"KAMU TIDAK BOLEH MENGENDARAI APA APA HAHAHAHAHA";
+
+        // System.out.println(hasil);
+
+
+        int umur = 21;
+        boolean member = true;
+        
+        if (umur > 21){
+            if (member == true){
+                System.out.println("selamat anda mendapat diskon seharga 1000 rupiah");
+                
+            } 
+            else{
+                System.out.println("anda mesti daftar dulu");
+            }
+        }    
+        else{
+            System.out.println("anda pasti dari sebelah yah");
+        }
+                
+            
+
+
+
+    }
+}
