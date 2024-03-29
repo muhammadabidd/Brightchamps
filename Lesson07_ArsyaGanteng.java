efbcee21d2e246e4c62bb0eef0e745ea5bf9f1b1
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-29 20:52:15 +0700

    minggu

---
+++ Lesson07_ArsyaGanteng.java
@@ -0,0 +1,17 @@
+public class Lesson07_ArsyaGanteng {
+
+    public static void main(String[] args) {
+        
+
+
+        for (int J = 1; J <= 10; J++) {
+            for (int i = 1; i <= J; i++) {
+                System.out.print("* ");
+            }
+
+            System.out.println("");
+        }
+
+
+    }
+}
