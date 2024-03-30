fb13790967d8d8e9f36bf576def187e6f95cdbf1
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-30 14:33:05 +0700

    update

---
+++ Lesson08_Arsya.java
@@ -0,0 +1,21 @@
+public class Lesson08_Arsya {
+    public static void main(String[] args) {
+        
+
+        int hitungan = 0;
+
+        while (true) {
+            hitungan = hitungan + 1;     //hitungan++
+
+            System.out.println("Hello "+ hitungan);
+
+            if (hitungan == 5){
+                break;
+            }
+
+
+        }
+
+
+    }
+}
