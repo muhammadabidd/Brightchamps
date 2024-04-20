91fdf57f96cd70cd4e287ac873f45eae5e25a3cc
Muhammad Abid <muhammmadabidd@gmail.com>
2024-04-20 17:00:11 +0700

    Lesson Today

--- Arsya_Lesson10_Palindrome.java
+++ Arsya_Lesson10_Palindrome.java
@@ -1,21 +1,46 @@
 public class Arsya_Lesson10_Palindrome {
     
 
-    // public static void cekpalindrome(String nama){
 
-
-
-        
-    // }
 
 
     public static void main(String[] args) {
-        
+     
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        if(cekpalindrome("KASURRUSAK")){
+            System.out.println("INI PALINDROME");
+        }
+        else{
+            System.out.println("Ini Bukan Palyndrome");
         }
 
+    }
+
+
+
+    public static boolean cekpalindrome(String nama){
+        String text = nama;
+        String maudicek = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+     
+
+            maudicek = maudicek + text.charAt(i);
+            System.out.println(maudicek);
+        }
+
+        if(text.equalsIgnoreCase(maudicek)){
+            System.out.println("SAMA SETELAH DIBALIK");
+
+            return true;
+        }
+        else{
+            System.out.println("TEXTNYA BEDA");
+            return false;
+        }
+
+
+        
     }
 
 
