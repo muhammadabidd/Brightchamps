4a30dcd4e5caea9e76099c377e79a799e740d21a
Muhammad Abid <muhammmadabidd@gmail.com>
2024-04-30 16:15:07 +0700

    java

---
+++ Java/Lesson15_SrotingMethods.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Lesson15_SrotingMethods {
+    
+
+
+    public static void main(String[] args) {
+        
+        int[] numbers = {22, 55, 11, 44, 33, 20};
+
+        Arrays.sort(numbers);
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+
+    }
+}
---
+++ Java/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+            if (array[i] > array[i+j]){
+                int temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+
+            }
