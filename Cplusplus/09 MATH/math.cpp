b3336b55c3f49e33e893fabfe7794861322fd771
muhammadabidd <muhammmadabidd@gmail.com>
2024-05-03 21:04:55 +0700

    save from computer

---
+++ Cplusplus/09 MATH/math.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+
+
+    int angle_degrees; 
+
+    cout << "Enter the angle in degrees: ";
+    cin >> angle_degrees;
+
+    cout << "The Angle in radian is : "  << angle_degrees *   M_PI / 180  << endl;
+
+
+
+
+
+
+
+
+    int powwer = pow(2,3);
+    cout << "This is example of power function" << endl;
+    cout << powwer << endl;
+
+
+    // "https://cplusplus.com/reference/cmath/"
+
+    return 0 ;
+}
