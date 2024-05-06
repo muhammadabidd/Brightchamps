2efe1f38b2e72c21f5df85bf4b1c8133865cb554
muhammadabidd <muhammmadabidd@gmail.com>
2024-05-07 05:47:27 +0700

    pagi pagi

--- Java/Lesson17_String_Password.java
+++ Java/Lesson17_String_Password.java
@@ -3,20 +3,32 @@ public class Lesson17_String_Password {
 
     public static void main(String[] args) {
 
-        String password = "ABCDEFGHIJKLMN";
+
+        String kombinasi1 = "ABCDEfghijklmn123+_)*";
+        String kombinasi2 = "ABCDEfghijklmn123+_)*<>";
+
+        String password = kombinasi2;
+
         char karakter ;
+
+
+        int lowCase = 0, Upcase = 0, digit = 0, special = 0;
         
         System.out.println("Password Rule : ");
-        System.out.println("Should at least 7 letters");
+        System.out.println("1. Should be at least 7 letters");
+
+        System.out.println(" 2. Should contain at least 1 Lowercase");
+        System.out.println(" 3. Should contain at least 1 Uppercase");
+        System.out.println(" 3. Should contain at least 1 Digit(0-9)");
 
 
         int passwordLen = password.length();
 
         if (passwordLen < 8) {
-            System.out.println("Password is not longth enought");
+            System.out.println("Password is not long enaugh ");
         }
         else{
-            System.out.println("password long enaught");
+            System.out.println("password long enaugh");
         }
 
 
@@ -24,8 +36,35 @@ public class Lesson17_String_Password {
             karakter =  password.charAt(i);
             System.out.println(karakter);
 
+            if (Character.isUpperCase(karakter)){
+                Upcase ++;
+                System.out.println("This IS Uppercase");
+
+            }
+            else if (Character.isLowerCase(karakter)) {
+                lowCase ++;
+            }
+            else if (Character.isDigit(karakter)){
+                digit ++;
+            }
+            else{
+                special ++;
+
+
+                if (karakter == '<' | karakter == '>') {
+                    System.out.println("This is Berbahaya");
+                    return;
+                }
+            }
+
         }
 
+        System.out.println("Uppercase : " + Upcase);
+        System.out.println("lowCase : " + lowCase);
+        System.out.println("digit : " + digit);
+        System.out.println("Special " + special);
+
+        
 
 
 
