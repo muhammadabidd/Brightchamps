480310382732057c8d06880456279114e3f0f9c5
muhammadabidd <muhammmadabidd@gmail.com>
2024-05-06 18:46:48 +0700

    update dulu

---
+++ Java/Lesson17_String_Password.java
@@ -0,0 +1,22 @@
+public class Lesson17_String_Password {
+    
+
+    public static void main(String[] args) {
+
+        String password = "ABCDEFGHIJKLMN";
+        char karakter ;
+        
+        System.out.println("Password Rule : ");
+        System.out.println("Should at least 7 letters");
+
+        for (String string : args) {
+            
+        }
+
+
+
+
+    }
+
+
+}
