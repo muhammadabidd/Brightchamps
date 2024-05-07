39f59fb8f76efa37cbf5eb0b97bc26a540bc0a70
muhammadabidd <muhammmadabidd@gmail.com>
2024-05-07 16:55:06 +0700

    habis ngajar arsya

--- Java/Lesson17_String_Password.java
+++ Java/Lesson17_String_Password.java
@@ -4,7 +4,7 @@ public class Lesson17_String_Password {
     public static void main(String[] args) {
 
 
-        String kombinasi1 = "ABCDEfghijklmn123+_)*";
+        // String kombinasi1 = "ABCDEfghijklmn123+_)*";
         String kombinasi2 = "ABCDEfghijklmn123+_)*<>";
 
         String password = kombinasi2;
@@ -29,35 +29,38 @@ public class Lesson17_String_Password {
         }
         else{
             System.out.println("password long enaugh");
-        }
 
 
-        for (int i = 0; i < passwordLen; i++) {
-            karakter =  password.charAt(i);
-            System.out.println(karakter);
-
-            if (Character.isUpperCase(karakter)){
-                Upcase ++;
-                System.out.println("This IS Uppercase");
-
-            }
-            else if (Character.isLowerCase(karakter)) {
-                lowCase ++;
-            }
-            else if (Character.isDigit(karakter)){
-                digit ++;
-            }
-            else{
-                special ++;
-
-
-                if (karakter == '<' | karakter == '>') {
-                    System.out.println("This is Berbahaya");
-                    return;
+            for (int i = 0; i < passwordLen; i++) {
+                karakter =  password.charAt(i);
+                System.out.println(karakter);
+    
+                if (Character.isUpperCase(karakter)){
+                    Upcase ++;
+                    System.out.println("This IS Uppercase");
+    
                 }
+                else if (Character.isLowerCase(karakter)) {
+                    lowCase ++;
+                }
+                else if (Character.isDigit(karakter)){
+                    digit ++;
+                }
+                else{
+                    special ++;
+    
+    
+                    if (karakter == '<' | karakter == '>') {
+                        System.out.println("This is Berbahaya");
+                        return;
+                    }
+                }
+    
             }
-
         }
+
+
+
 
         System.out.println("Uppercase : " + Upcase);
         System.out.println("lowCase : " + lowCase);
@@ -66,7 +69,42 @@ public class Lesson17_String_Password {
 
         
 
+        if (Upcase != 0 && lowCase != 0 && digit != 0 && special != 0){
 
+            System.out.println("The password valid");
+
+
+            if (passwordLen > 12){
+                System.out.println("Password is strong");
+            }
+            else{
+                System.out.println("password is not long enaugh");
+            }
+
+
+        }
+        else{
+
+            System.out.println("The Password is invalid");
+
+
+            if (Upcase == 0) {
+                System.out.println("PLease insert at least 1 uppercase");
+            }
+            
+            if (lowCase == 0) {
+                System.out.println("PLease insert at least 1 lowercase");
+            }
+            
+            if (digit == 0) {
+                System.out.println("PLease insert at least 1 digit");
+            }
+            
+            if (special == 0) {
+                System.out.println("PLease insert at least 1 special character");
+            }
+        }
+        
 
     }
 
---
+++ Java/Lesson17_String_textLen.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Lesson17_String_textLen {
+    public static void main(String[] args) {
+
+       String word ="";
+       String longestword = "";
+
+       int len = 0;
+       int max_len = 0;
+
+       String mytext = "aksdjalsdkjalsdkjalsdkjalsdkj abid suka makan makanan ";
+
+       String kata_kata = mytext.trim();
+
+       System.out.println("mytext original terdiri dari :" + mytext.length());
+       System.out.println("mytext setelah di trim terdiri dari :" + kata_kata.length());
+
+       for (int i = 0; i < kata_kata.length(); i++) {
+              char karakter = kata_kata.charAt(i);
+              System.out.println(karakter);
+
+
+              if (karakter != ' '){
+              
+                     word = word + karakter; 
+              }
+              else{
+                     len = word.length();
+
+                     if (len > max_len){
+                            longestword = word;
+                            max_len = len;
+                     }
+                     
+                     
+                     word = "";
+              }
+       }
+
+       System.out.println(longestword);
+
+    }
+}
