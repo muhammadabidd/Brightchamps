a1aaeee4773b242ee7cb116c96f0adb7a17e5a7e
Muhammad Abid <muhammmadabidd@gmail.com>
2024-08-12 13:12:19 +0700

    finish cpp

--- Cplusplus/18 aRRAY/Hangman.cpp
+++ Cplusplus/18 aRRAY/Hangman.cpp
@@ -4,10 +4,15 @@ using namespace std;
 int main()
 {
     const int MAX_GUESSES = 6;
+    const string WORDS[] = {"computer", "programming", "Algorithm", "Binary", "Variable", "Loop"};
+    const int NUM_WORDS = 6;
     int numGuesses = 0;
 
-    string word = "computer";
+    string word = WORDS[rand() % NUM_WORDS];       // Selected word : Loop
     int wordLength = word.length();
+    string hiddenWord(wordLength, '_');
+    cout << hiddenWord;
+
 
     cout << "Welcome to Hangman!" << endl;
     cout << "The word has " << wordLength << " letters." << endl;
@@ -17,13 +22,34 @@ int main()
         char guess;
         cin >> guess;
 
-        numGuesses++;
+
+        bool found = false;
+
+        for (int i = 0; i < wordLength; i++) {
+            if (word[i] == guess) {
+                hiddenWord[i] = guess;
+                found = true;
+                cout << "this is match" << endl;
+            }
+        }
+
+        if (!found){
+                numGuesses++;   
+                cout << "INCORRECT, " ;
+        }
+
+
         cout << "You have " << MAX_GUESSES - numGuesses << " guesses left." << endl;
         // cout << "You have guessed this many time:" << numGuesses<< endl;
 
-    }while (numGuesses < MAX_GUESSES);
+        cout << "The word : " <<   hiddenWord << endl;
+    }while (numGuesses < MAX_GUESSES && hiddenWord != word);
 
-    
+    if (hiddenWord == word) {
+        cout << "Congratulations, you won!" << endl;
+    } else {
+        cout << "Sorry, you lost. The word was " << word << "." << endl;
+    }
 
 return 0;
 }
