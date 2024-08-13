9d8bb82d577724535632abc3e8a278c4092cac48
Muhammad Abid <muhammmadabidd@gmail.com>
2024-08-13 22:01:15 +0700

    m

---
+++ Cplusplus/18 aRRAY/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+ // int scoreA = 90;
+    // int scoreB = 80;
+    // int scoreC = 99;
+    // int scoreD = 80;
+    // int scoreE = 88;
+
+
+    // int total = scoreA + scoreB + scoreC + scoreD + scoreE;
+
+    // double average = total/5;
+
+    // cout << average << endl; 
