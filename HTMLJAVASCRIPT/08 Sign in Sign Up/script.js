2c7e0c1a2b5d04b4baba67b3460938eaf9fde29a
Muhammad Abid <muhammmadabidd@gmail.com>
2024-08-17 20:59:27 +0700

    half finish like budi

--- HTMLJAVASCRIPT/08 Sign in Sign Up/script.js
+++ HTMLJAVASCRIPT/08 Sign in Sign Up/script.js
@@ -1,1 +1,18 @@
-console.log("Aboid")
+console.log("Abid")
+
+
+signUpButton = document.querySelector("#signUpButton")
+
+// signUpButton.addEventListener('click', function(){
+//     console.log("This is addEventListener")
+// })
+
+
+signUpButton.onclick = function(){
+    console.log("this is onclick")
+    
+    registName = document.getElementById("registName").value;
+    registEmail = document.getElementById("registEmail").value;
+    registPassword = document.getElementById("registPassword").value;
+    
+}
