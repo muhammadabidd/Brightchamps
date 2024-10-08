aa4896ca576384dd24bf4661b9a644b6de37c97b
Muhammad Abid <muhammmadabidd@gmail.com>
2024-10-08 21:53:46 +0700

    ss

--- HTMLJAVASCRIPT/09 Resturant Menu/EzzarClass/script.js
+++ HTMLJAVASCRIPT/09 Resturant Menu/EzzarClass/script.js
@@ -32,3 +32,23 @@ const menu = [
    diplayMenuItems(menu);
    displayMenuButtons();
  });
+
+
+ function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+ displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+}
